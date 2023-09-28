fc5b7e8d606a5982542fa329b178084df1790882
Sweta Gupta <122213106+Sweta1G@users.noreply.github.com>
2023-09-28 10:06:55 +0530

    Time: 1 ms (81.28%), Space: 43.9 MB (74.25%) - LeetHub

--- 0905-sort-array-by-parity/0905-sort-array-by-parity.java
+++ 0905-sort-array-by-parity/0905-sort-array-by-parity.java
@@ -4,14 +4,10 @@ class Solution {
         int[] arr= new int[nums.length];
         int j=0;
         for(int i=0; i<nums.length; i++){
-            if(nums[i]%2==0){
-                arr[j++]=nums[i];
-            }
+            if(nums[i]%2==0)   arr[j++]=nums[i];
         }
         for(int i=0; i<nums.length; i++){
-            if(nums[i]%2!=0){
-                arr[j++]=nums[i];
-            }
+            if(nums[i]%2!=0)   arr[j++]=nums[i];
         }
         return arr;
     }
