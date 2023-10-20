4a53b65681a6181721f4f4fe04a2c048fbab4755
Sweta Gupta <122213106+Sweta1G@users.noreply.github.com>
2023-10-20 08:35:20 +0530

    Time: 8 ms (22.58%), Space: 39.2 MB (43.20%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,12 +1,5 @@
 class Solution {
     private int findFib(int n){
-        // int first=0, second=1, sum=0, count=0;
-        // while(count < n){
-        //     int temp= first+second;
-        //     first= second;
-        //     second= temp;
-        //     sum+= temp;
-        // }
         int sum=0;
         if(n==0) return 0;
         if(n==1) return 1;
@@ -14,10 +7,8 @@ class Solution {
         return sum;
     }
     public int fib(int n) {
-        // if(n==0 || n==1) return 0;
-        // if(n==2) return 1;
-        int ans= findFib(n);
-        return ans;
+        // int ans= findFib(n);
+        return findFib(n);
         
     }
 }
