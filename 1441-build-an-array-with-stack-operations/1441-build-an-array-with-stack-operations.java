7b1bc7594a6d27a3467f30ecca563360f2496cd3
Sweta Gupta <122213106+Sweta1G@users.noreply.github.com>
2023-11-03 20:28:09 +0530

    Time: 0 ms (100.00%), Space: 41.8 MB (48.81%) - LeetHub

--- 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.java
+++ 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.java
@@ -1,16 +1,16 @@
 class Solution {
     public List<String> buildArray(int[] t, int n) {
         List<String> ans= new ArrayList();
-        if(t.length==1){
-            int i=1;
-            while(i != t[0]){
-                ans.add("Push");
-                ans.add("Pop");
-                i++;
-            }
-            ans.add("Push");
-            return ans;
-        }
+        // if(t.length==1){
+        //     int i=1;
+        //     while(i != t[0]){
+        //         ans.add("Push");
+        //         ans.add("Pop");
+        //         i++;
+        //     }
+        //     ans.add("Push");
+        //     return ans;
+        // }
         int num=1, i=0;
         do{
             ans.add("Push");
