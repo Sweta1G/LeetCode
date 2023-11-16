82c93ae876983d0385ff40f98aa09da5cbe03086
Sweta Gupta <122213106+Sweta1G@users.noreply.github.com>
2023-11-16 09:06:33 +0530

    Time: 0 ms (100.00%), Space: 40.5 MB (56.91%) - LeetHub

--- 1980-find-unique-binary-string/1980-find-unique-binary-string.java
+++ 1980-find-unique-binary-string/1980-find-unique-binary-string.java
@@ -1,17 +1,11 @@
 class Solution {
     public String findDifferentBinaryString(String[] nums) {
+        
         StringBuilder result = new StringBuilder();
-
-        // Iterate through each binary string in the array
         for (int i = 0; i < nums.length; ++i) {
-            // Access the i-th character of the i-th binary string
             char currentCharacter = nums[i].charAt(i);
-
-            // Append the opposite binary value to the result string
             result.append((currentCharacter == '0' ? '1' : '0'));
         }
-
-        // Return the resulting binary string
         return result.toString();
     }
 }
