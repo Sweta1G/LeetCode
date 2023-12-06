f396b1515b93883454de0fd96a726c1d347e1a3f
Sweta Gupta <122213106+Sweta1G@users.noreply.github.com>
2023-12-06 22:43:22 +0530

    Time: 1 ms (60.95%), Space: 39.3 MB (43.33%) - LeetHub

--- 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.java
+++ 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.java
@@ -1,9 +1,8 @@
 class Solution {
     public int totalMoney(int n) {
         int sum=0; 
-        for(int i=0; i<n; i++){
+        for(int i=0; i<n; i++)
             sum+= (i/7)+1+(i%7);
-        }
         return sum;
     }
 }
