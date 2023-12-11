0a59ee95f932a12fd674f0e6be51bd984c2ebfe1
Sweta Gupta <122213106+Sweta1G@users.noreply.github.com>
2023-12-11 20:45:56 +0530

    Time: 14 ms (13.11%), Space: 44.6 MB (8.23%) - LeetHub

--- 1287-element-appearing-more-than-25-in-sorted-array/1287-element-appearing-more-than-25-in-sorted-array.java
+++ 1287-element-appearing-more-than-25-in-sorted-array/1287-element-appearing-more-than-25-in-sorted-array.java
@@ -5,7 +5,6 @@ class Solution {
             if(map.containsKey(arr[i])) map.put(arr[i], map.get(arr[i])+1);
             else map.put(arr[i],1);
         }
-        // return Collections.max(map.values());
         return Collections.max(map.entrySet(), Map.Entry.comparingByValue()).getKey();
     }
 }
