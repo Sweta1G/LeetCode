b73f9d72e38d8bdb4b9b2d2a007ce95317ad881a
Sweta Gupta <122213106+Sweta1G@users.noreply.github.com>
2024-01-31 10:41:35 +0530

    Time: 64 ms (53.82%), Space: 63.4 MB (5.12%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.java
+++ 0739-daily-temperatures/0739-daily-temperatures.java
@@ -1,6 +1,5 @@
 class Solution {
     public int[] dailyTemperatures(int[] temperatures) {
-        
         Stack<Integer> stack = new Stack<Integer>();
         int[] ans = new int[temperatures.length];
         for (int i=0; i < temperatures.length; i++) {
@@ -11,22 +10,5 @@ class Solution {
             stack.push(i);
         }
         return ans;
-        
-        
-//         int ans[]= new int[temp.length], l=0;
-        
-//         for(int i=0; i<temp.length; i++){
-            
-//             int c=1, today= temp[i];
-//             for(int j=i+1; j<temp.length; j++){
-//                 if(today<temp[j]) {
-//                     ans[l++]= c;
-//                     break;
-//                 }
-//                 if(j==temp.length-1) ans[l++]=0;
-//                 else c++;
-//             }
-//         }
-//         return ans;
     }
 }
