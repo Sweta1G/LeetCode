bd85e0ab31e7c0382028d03993d2eac5e0f5fbd6
Sweta Gupta <122213106+Sweta1G@users.noreply.github.com>
2024-04-02 09:40:23 +0530

    Time: 8 ms (78.67%), Space: 42 MB (94.31%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        if(s.length()!=t.length()) return false;
+        int[] freqS = new int[200]; 
+        int[] freqT = new int[200];
+        
+        for(int i = 0; i < s.length(); i++) {
+            if(freqS[s.charAt(i)] != freqT[t.charAt(i)])
+                return false;
+            
+            freqS[s.charAt(i)] = i + 1;
+            freqT[t.charAt(i)] = i + 1;
+        }
+        return true;
+    }
+}
