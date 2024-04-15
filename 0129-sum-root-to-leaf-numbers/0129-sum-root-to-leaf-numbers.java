c2161f9b990a063746f5e1ddefa4cec226d41bd4
Sweta Gupta <122213106+Sweta1G@users.noreply.github.com>
2024-04-15 12:38:36 +0530

    Time: 0 ms (100.00%), Space: 40.8 MB (86.84%) - LeetHub

--- 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.java
+++ 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.java
@@ -20,9 +20,10 @@ class Solution {
         res= root.val+ res*10;
         if(root.left==null && root.right==null) return res;
         
-        return helper(root.left,res)+helper(root.right,res);
+        return helper(root.left,res) + helper(root.right,res);
     }
-    public int sumNumbers(TreeNode root) {
+    public int sumNumbers(TreeNode root){
         return helper(root,0);
     }
+    
 }
