2218ee832ac09aee66efc81583c2629e1e02f5d6
Sweta Gupta <122213106+Sweta1G@users.noreply.github.com>
2024-05-04 09:11:33 +0530

    Time: 16 ms (97.17%), Space: 53.2 MB (85.10%) - LeetHub

--- 0881-boats-to-save-people/0881-boats-to-save-people.java
+++ 0881-boats-to-save-people/0881-boats-to-save-people.java
@@ -5,9 +5,9 @@ class Solution {
         int boats=0, s=0;
         for(int i=people.length-1; i>=s-0; i--){
             if(people[i]==limit) boats++;
-            else if(people[i]<limit){
-                if(s<people.length && people[s]+people[i]<=limit) {boats++;s++;}
-                else boats++;
+            else{
+                if(s<people.length && people[s]+people[i]<=limit) s++;
+                boats++;
             }
         }
         return boats;
