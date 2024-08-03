c8c2dc1e166b23c9498b33c443118983fd03b2ad
Sweta Gupta <122213106+Sweta1G@users.noreply.github.com>
2024-08-03 18:27:01 +0530

    Time: 4 ms (54.32%), Space: 44 MB (86.16%) - LeetHub

--- 1460-make-two-arrays-equal-by-reversing-subarrays/1460-make-two-arrays-equal-by-reversing-subarrays.java
+++ 1460-make-two-arrays-equal-by-reversing-subarrays/1460-make-two-arrays-equal-by-reversing-subarrays.java
@@ -1,21 +1,12 @@
 class Solution {
-    private boolean equalElements(int[] t, int[] arr, int n){
-        Arrays.sort(t);
-        Arrays.sort(arr);
-        for(int i=0; i<n; i++)
-            if(arr[i]!=t[i]) return false;
-        return true;
-    }
+
     public boolean canBeEqual(int[] target, int[] arr) {
         int n= arr.length;
-        // if(n==1 && arr[0]==target[0]) return true;
-        if(!equalElements(target,arr,n)) return false;
-        return true;
         
-        
-        
-        
-        
-        
+        Arrays.sort(target);
+        Arrays.sort(arr);
+        for(int i=0; i<n; i++)
+            if(arr[i]!=target[i]) return false;
+        return true; 
     }
 }
