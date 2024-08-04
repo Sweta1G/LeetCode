07b8cd413f6f578d26f7c938b1740f8cdf732505
Sweta Gupta <122213106+Sweta1G@users.noreply.github.com>
2024-08-04 10:27:02 +0530

    Time: 66 ms (68.47%), Space: 46.7 MB (43.69%) - LeetHub

--- 1508-range-sum-of-sorted-subarray-sums/1508-range-sum-of-sorted-subarray-sums.java
+++ 1508-range-sum-of-sorted-subarray-sums/1508-range-sum-of-sorted-subarray-sums.java
@@ -2,13 +2,21 @@ class Solution {
     public int rangeSum(int[] nums, int n, int left, int right) {
         int sums[]= new int[n*(n+1)/2], l=0;
         
-        for(int i=0; i<n; i++){
-            for(int j=i; j<n; j++){
-                int sum=0;
-                for(int k=i; k<=j; k++){
-                    sum+= nums[k];
-                }
-                if(l<sums.length)
+        // for(int i=0; i<n; i++){
+        //     for(int j=i; j<n; j++){
+        //         int sum=0;
+        //         for(int k=i; k<=j; k++){
+        //             sum+= nums[k];
+        //         }
+        //         if(l<sums.length)
+        //             sums[l++]= sum;
+        //     }
+        // }
+        for (int i = 0; i < n; i++) {
+            int sum = 0;
+            for (int j = i; j < n; j++) {
+                sum += nums[j];
+                if(l<sums.length);
                     sums[l++]= sum;
             }
         }
