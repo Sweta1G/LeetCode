070c1d69ec59bb98a61b12c2050f0e97f0cdb650
Sweta Gupta <122213106+Sweta1G@users.noreply.github.com>
2024-08-12 11:21:08 +0530

    Time: 4 ms (11.87%), Space: 46.5 MB (15.91%) - LeetHub

---
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int trap(int[] height) {
+        int n= height.length;
+        // int ml=0, mr=0;
+        List<Integer> left= new ArrayList<>(); left.add(height[0]);
+        List<Integer> right= new ArrayList<>(); right.add(height[n-1]); int t=0;
+        
+        for(int i=1; i<n; i++){
+            left.add(Math.max(left.get(i-1),height[i]));
+        }
+        for(int i=n-2; i>=0; i--){
+            right.add(Math.max(right.get(t++),height[i]));
+        }
+        Collections.reverse(right);
+        
+        int area=0;
+        for(int i=0; i<n; i++){
+            int h= Math.min(right.get(i),left.get(i))-height[i];
+            area+= h;
+        }
+        return area;
+    }
+}
