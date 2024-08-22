f203cacac4972fe1ed4899217f32ade7a458b674
Sweta Gupta <122213106+Sweta1G@users.noreply.github.com>
2024-08-22 10:29:48 +0530

    Time: 0 ms (100.00%), Space: 40.7 MB (20.00%) - LeetHub

--- 0476-number-complement/0476-number-complement.java
+++ 0476-number-complement/0476-number-complement.java
@@ -2,8 +2,8 @@ class Solution {
     public int findComplement(int n) {
         int res= 0, power= 0;
         while(n>0){
-            int last= n%2;
-            int c= last==0? 1:0;
+            // int last= n%2;
+            int c= n%2==0? 1:0;
             res+= (c * (int) Math.pow(2,power));
             power++;
             n/= 2;
