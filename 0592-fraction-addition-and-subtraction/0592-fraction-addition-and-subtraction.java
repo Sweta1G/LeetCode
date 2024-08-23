133072d62ad760ba45ceb5be96c5f9333b0900df
Sweta Gupta <122213106+Sweta1G@users.noreply.github.com>
2024-08-23 12:22:11 +0530

    Time: 7 ms (54.59%), Space: 41.8 MB (70.18%) - LeetHub

---
+++ 0592-fraction-addition-and-subtraction/0592-fraction-addition-and-subtraction.java
@@ -0,0 +1,60 @@
+class Solution {
+    private String complement(String s){
+        char c= s.charAt(0)=='-'? '+':'-';
+        s= c+s.substring(1);
+        return s;
+    }
+    private int __gcd(int a, int b){  
+        return b == 0? a:__gcd(b, a % b);     
+    }
+    private int LcmOfArray(int[] arr, int idx){
+        if (idx == arr.length - 1)
+            return arr[idx];
+        int a = arr[idx];
+        int b = LcmOfArray(arr, idx+1);
+        return (a*b/__gcd(a,b)); 
+    }
+    public String fractionAddition(String expression) {
+        String exp[]= expression.split("(?=[+-])");
+        int n= exp.length;
+        
+//         Set<String> set= new HashSet<>();
+//         set.add(parts[0]);
+        
+//         for(int i=1; i<n; i++){
+//             String exp= parts[i];
+//             if(set.contains(complement(exp))) set.remove(complement(exp));
+//             else set.add(exp);
+//         }
+//         if(set.size()==0) return "0/1";
+        // n= set.size();
+        // String[] exp = set.toArray(new String[n]);
+        for(int i=0; i<n; i++)
+                if(exp[i].charAt(0)=='+') exp[i]= exp[i].substring(1);
+        if(n==1) 
+            return exp[0];
+        
+        
+        int dino[]= new int[n];
+        int nume[]= new int[n];
+        
+        for(int i=0; i<n; i++){
+            String s[]= exp[i].split("/");
+            nume[i]= Integer.parseInt(s[0]);
+            dino[i]= Integer.parseInt(s[1]);
+        }
+        int num= 0;
+        int lcm= LcmOfArray(dino,0);
+        for(int i=0; i<n; i++){
+            nume[i]*= (lcm/dino[i]);
+            num+= nume[i];
+        }
+        for(int i=2; i<=lcm; i++){
+            while(num%i== 0 && lcm%i== 0){
+                num/= i;
+                lcm/= i;
+            }
+        }
+        return String.valueOf(num)+"/"+String.valueOf(lcm);
+    }
+}
