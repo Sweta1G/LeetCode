1fd08fa9c674607627cb039b28bc927feb04d5e4
Sweta Gupta <122213106+Sweta1G@users.noreply.github.com>
2024-08-23 21:29:46 +0530

    Time: 0 ms (100.00%), Space: 40.9 MB (81.78%) - LeetHub

--- 2335-minimum-amount-of-time-to-fill-cups/2335-minimum-amount-of-time-to-fill-cups.java
+++ 2335-minimum-amount-of-time-to-fill-cups/2335-minimum-amount-of-time-to-fill-cups.java
@@ -1,54 +1,10 @@
-class Pair{
-    int ele;
-    char c;
-    Pair(int e, char c){
-        this.ele= e;
-        this.c= c;
-    }
-}
 class Solution {
-    public class PairComparator implements Comparator<Pair>{
-        public int compare(Pair p1, Pair p2){
-            return p2.ele-p1.ele;
+    public int fillCups(int[] A) {
+        int mx = 0, sum = 0;
+        for(int a: A) {
+            mx = Math.max(a, mx);
+            sum += a;
         }
-    }
-    public int fillCups(int[] amount) {
-        PriorityQueue<Pair> pq= new PriorityQueue<>(new PairComparator());
-        
-        for(int i=0; i<amount.length; i++){
-            if(i==0){
-                if(amount[i]==0) continue;
-                pq.add(new Pair(amount[i],'c'));
-            }
-            else if(i==1){
-                if(amount[i]==0) continue;
-                pq.add(new Pair(amount[i],'w'));
-            }
-            else if(i==2){
-                if(amount[i]==0) continue;
-                pq.add(new Pair(amount[i],'h'));
-            }
-        }
-                                    
-        
-        int time= 0;
-        while(pq.size()>1){
-            // time++;
-            Pair x= pq.poll();
-            Pair y= pq.poll();
-            
-            if(x.ele == 0 && y.ele== 0) break;
-            else if(y.ele==0)
-                pq.add(new Pair(x.ele-1,x.c));
-            else if(x.ele == 0)
-                pq.add(new Pair(y.ele-1,y.c));
-            else{
-                pq.add(new Pair(x.ele-1, x.c));
-                pq.add(new Pair(y.ele-1, y.c));
-            }
-            time++;
-        }   
-        if(pq.size()==1) time+= pq.peek().ele;
-        return time;
+        return Math.max(mx, (sum + 1) / 2);
     }
 }
