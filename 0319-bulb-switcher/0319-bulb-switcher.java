ab17905d773a02d8dbb488ace0022ea9d13f79d9
Sweta Gupta <122213106+Sweta1G@users.noreply.github.com>
2024-08-29 01:05:00 +0530

    Time: 0 ms (100.00%), Space: 40.4 MB (37.86%) - LeetHub

---
+++ 0319-bulb-switcher/0319-bulb-switcher.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int bulbSwitch(int n) {
+        if(n==0 || n==1) return n;
+        return (int)Math.sqrt(n);
+        
+    }
+    // 1 1 1 1 1
+    // 1 0 1 0 1
+    // 0 1 0 1 0
+    
+    //
+}
