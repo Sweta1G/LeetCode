030777afcf74286b134ae79853601281c48000cf
Sweta Gupta <122213106+Sweta1G@users.noreply.github.com>
2024-08-28 15:11:40 +0530

    Time: 65 ms (26.19%), Space: 45.9 MB (11.33%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
@@ -1,15 +1,19 @@
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
         List<Integer> res= new ArrayList<>();
-        char ana[]= p.toCharArray();
-        Arrays.sort(ana);
         
         int i=0, j=0, k= p.length();
+        HashMap<Character,Integer> mp= new HashMap<>();
+        for(int l=0; l< k; l++)
+            mp.put(p.charAt(l),mp.getOrDefault(p.charAt(l),0)+1);
+        
+        HashMap<Character,Integer> temp= new HashMap<>();
         while(j<s.length()){
+            temp.put(s.charAt(j),temp.getOrDefault(s.charAt(j), 0)+1);
             if(j-i+1==k){
-                char tmp[]= s.substring(i,j+1).toCharArray();
-                Arrays.sort(tmp);
-                if(Arrays.equals(tmp,ana)) res.add(i);
+                if(mp.equals(temp)) res.add(i);
+                temp.put(s.charAt(i),temp.get(s.charAt(i))-1);
+                if(temp.get(s.charAt(i))==0) temp.remove(s.charAt(i));
                 i++;
             }
             j++;
