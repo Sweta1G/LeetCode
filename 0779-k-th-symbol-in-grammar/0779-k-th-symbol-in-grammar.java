d37d7273558c04cfedb4efd72e9f07b321a35a95
Sweta Gupta <122213106+Sweta1G@users.noreply.github.com>
2024-08-31 12:47:08 +0530

    Time: 0 ms (100.00%), Space: 40.1 MB (86.94%) - LeetHub

--- 0779-k-th-symbol-in-grammar/0779-k-th-symbol-in-grammar.java
+++ 0779-k-th-symbol-in-grammar/0779-k-th-symbol-in-grammar.java
@@ -1,26 +1,12 @@
-import java.lang.Math;
+
 class Solution {
     public int kthGrammar(int n, int k) {
+        if(n==1 && k==1) return 0;
+        int mid= (int) Math.pow(2,n-1)/2;
         
-boolean areValuesSame = true;
-
-        // Calculate the total number of elements in the nth row, which is 2^(n-1).
-        n = (int) Math.pow(2, n - 1);
-
-        // Continue until we reach the first row.
-        while (n != 1) {
-            // Halve the number of elements in the row.
-            n /= 2;
-
-            // If k is in the second half of the row, adjust k and toggle the flag.
-            if (k > n) {
-                k -= n;
-                areValuesSame = !areValuesSame;
-            }
-        }
-
-        // Return 0 if the flag indicates that the values are the same; otherwise, return 1.
-        return (areValuesSame ? 0 : 1);
+        if(k<=mid) return kthGrammar(n-1,k);
+        else 
+            return kthGrammar(n-1,k-mid)==0? 1 : 0;
         
     }
 }
