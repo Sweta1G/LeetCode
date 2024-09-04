7a8ea9d576883effd01df53e8af5b68c17fa2d9c
Sweta Gupta <122213106+Sweta1G@users.noreply.github.com>
2024-09-04 09:51:28 +0530

    Time: 34 ms (53.27%), Space: 48.4 MB (95.63%) - LeetHub

--- 0874-walking-robot-simulation/0874-walking-robot-simulation.java
+++ 0874-walking-robot-simulation/0874-walking-robot-simulation.java
@@ -1,76 +1,126 @@
-// class Pair{
-//     int x,y;
-//     Pair(int x, int y){
-//         this.x=x;
-//         this.y=y;
-//     }
-//     @Override
-//     public boolean equals(Object o) {
-//         if (this == o) return true;
-//         if (o == null || getClass() != o.getClass()) return false;
-//         Pair pair = (Pair) o;
-//         return x == pair.x && y == pair.y;
-//     }
-
-//     @Override
-//     public int hashCode() {
-//         return Objects.hash(x, y);
-//     }
-// }
 class Solution {
-    public int robotSim(int[] commands, int[][] obstacles) {
-        int d= -1;
-        int i=0, j=0, n= commands.length;
-        HashSet<String> set= new HashSet<>();
-        if(obstacles.length>=0){
-            for(int[] obs : obstacles)
-                set.add(obs[0]+","+obs[1]);
-            // for (Pair p : set) 
-            //     System.out.println("X: "+p.x+"  Y: "+p.y);
+//     public int robotSim(int[] commands, int[][] obstacles) {
+//         int d= -1;
+//         int i=0, j=0, n= commands.length;
+//         HashSet<String> set= new HashSet<>();
+//         if(obstacles.length>=0){
+//             for(int[] obs : obstacles)
+//                 set.add(obs[0]+","+obs[1]);
+//             // for (Pair p : set) 
+//             //     System.out.println("X: "+p.x+"  Y: "+p.y);
             
+//         }
+//         System.out.println("HashSet Size: "+set.size());
+//         System.out.println("HashSet: ");
+        
+        
+//         // commands[]= {4,-1,4,-2,4};
+//         char c= 'n';
+//         for(int t=0; t<n; t++){
+//             // System.out.println("Start of Loop in direction: "+c+" at ("+i+","+j+") with distance: "+d);
+//             if(commands[t]==-2){
+//                 if(c== 'n') c= 'w';
+//                 else if(c== 'w') c= 's';
+//                 else if(c== 's') c= 'e';
+//                 else c= 'n';
+//             }
+//             else if(commands[t]==-1){
+//                 if(c== 'n') c= 'e';
+//                 else if(c== 'e') c= 's';
+//                 else if(c== 's') c= 'w';
+//                 else c= 'n';
+//             }
+//             else{
+//                 int z= commands[t];
+//                 while(z-- > 0){
+//                     if(c=='n') j++;
+//                     else if(c=='e') i++;
+//                     else if(c=='s') j--;
+//                     else i--;
+//                     if(set.size()>0 && set.contains(i+","+j)){ 
+//                         // System.out.println("hdfkjedfb");
+//                         if(c=='n') j--;
+//                         else if(c=='e') i--;
+//                         else if(c=='s') j++;
+//                         else i++;
+//                         break;
+//                     }
+//                 }
+//             }
+//             int calc= Math.abs((i*i)+(j*j)); System.out.println("Calculated Distance: "+calc);
+//             d= Math.max(d,calc);
+//             System.out.println("Currently in direction: "+c+" at ("+i+","+j+") with distance: "+d);
+//         }
+        
+//         return d;
+//     }
+    public int robotSim(int[] commands, int[][] obstacles) {   
+        int dir = 0; // states 0north-1east-2south-3west
+        int farthestSofar = 0;
+        
+        int xloc = 0;
+        int yloc = 0;
+        
+        Set<String> set = new HashSet<>();
+        for (int[] obs : obstacles) {
+            set.add(obs[0] + "," + obs[1]);
         }
-        System.out.println("HashSet Size: "+set.size());
-        System.out.println("HashSet: ");
         
+        int steps; 
         
-        // commands[]= {4,-1,4,-2,4};
-        char c= 'n';
-        for(int t=0; t<n; t++){
-            // System.out.println("Start of Loop in direction: "+c+" at ("+i+","+j+") with distance: "+d);
-            if(commands[t]==-2){
-                if(c== 'n') c= 'w';
-                else if(c== 'w') c= 's';
-                else if(c== 's') c= 'e';
-                else c= 'n';
+        for(int i: commands){
+            
+            if( i == -1){//turn right 90
+                dir++;
             }
-            else if(commands[t]==-1){
-                if(c== 'n') c= 'e';
-                else if(c== 'e') c= 's';
-                else if(c== 's') c= 'w';
-                else c= 'n';
+            else if (i == -2){//turn left 90
+                dir--;
             }
-            else{
-                int z= commands[t];
-                while(z-- > 0){
-                    if(c=='n') j++;
-                    else if(c=='e') i++;
-                    else if(c=='s') j--;
-                    else i--;
-                    if(set.size()>0 && set.contains(i+","+j)){ 
-                        // System.out.println("hdfkjedfb");
-                        if(c=='n') j--;
-                        else if(c=='e') i--;
-                        else if(c=='s') j++;
-                        else i++;
-                        break;
+            else{//move forward value of i baring no obsticals
+                dir = dir%4;
+                if (dir== -1){
+                    dir = 3;
+                }
+                else if(dir == -3){
+                    dir = 1;
+                }
+                else if(dir == -2){
+                    dir = 2;
+                }
+                // dir %4 = -1 -> 3
+                // dir %4 = -2 -> 2
+                // dir %4 = -3 -> 1
+                if(dir == 0){
+                    steps = 0;
+                    while (steps < i && !set.contains((xloc) + "," + (yloc+1))) {
+                    yloc++;
+                    steps++;
+                    }
+                }
+                else if (dir == 1){
+                    steps = 0;
+                    while (steps < i && !set.contains((xloc+1) + "," + (yloc))) {
+                    xloc++;
+                    steps++;
+                    }
+                }
+                else if (dir == 2){
+                    steps = 0;
+                    while (steps < i && !set.contains((xloc) + "," + (yloc-1))) {
+                    yloc--;
+                    steps++;
+                    }
+                }
+                else{ //case dir == 3
+                    steps = 0;
+                    while (steps < i && !set.contains((xloc-1) + "," + (yloc))) {
+                    xloc--;
+                    steps++;
                     }
                 }
             }
-            int calc= Math.abs((i*i)+(j*j)); System.out.println("Calculated Distance: "+calc);
-            d= Math.max(d,calc);
-            System.out.println("Currently in direction: "+c+" at ("+i+","+j+") with distance: "+d);
+            farthestSofar = Math.max(farthestSofar, xloc*xloc + yloc*yloc);
         }
-        
-        return d;
+        return farthestSofar;
     }
 }
