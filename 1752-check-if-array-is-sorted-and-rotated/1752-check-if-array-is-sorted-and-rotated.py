4f2e86ab4bda259d4fe87ff713d8a79f5c03987f
Sweta Gupta <122213106+Sweta1G@users.noreply.github.com>
2024-10-04 16:37:16 +0530

    Time: 21 ms (30.08%), Space: 11.6 MB (70.08%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.py
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.py
@@ -1,12 +1,18 @@
-class Solution {
-public:
-    bool check(vector<int>& nums) {
-        int count= 0, n= nums.size();
+class Solution(object):
+    def check(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        count = 0
+        n = len(nums)
+
+        for i in range(1, n):
+            if nums[i-1] > nums[i]:
+                count += 1
+
+        if nums[n-1] > nums[0]:
+            count += 1
+
+        return count == 1 or count == 0
         
-        for(int i=1; i<n; i++){
-            if(nums[i-1]>nums[i]) count++;
-        }
-        if(nums[n-1]>nums[0]) count++;
-        return count==1 || count==0;
-    }
-};
