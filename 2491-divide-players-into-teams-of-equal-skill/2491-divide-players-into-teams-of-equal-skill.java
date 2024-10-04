00b984951897ce2e7f7f705cce4a204eb33934fd
Sweta Gupta <122213106+Sweta1G@users.noreply.github.com>
2024-10-04 13:21:29 +0530

    Time: 3 ms (96.47%), Space: 55.6 MB (94.21%) - LeetHub

--- 2491-divide-players-into-teams-of-equal-skill/2491-divide-players-into-teams-of-equal-skill.java
+++ 2491-divide-players-into-teams-of-equal-skill/2491-divide-players-into-teams-of-equal-skill.java
@@ -2,22 +2,26 @@ class Solution {
     public long dividePlayers(int[] skill) {
         int n= skill.length;
         if(n==2) return (long) skill[0]*skill[1];
+        int[] freq= new int[1001];
+        
         int teams= n/2;
         int total= 0;
-        for(int i:skill)
+        for(int i:skill){
             total+=i;
+            freq[i]++;
+        }
+        if(total % (n/2)!=0) return -1;
         int req= total/teams;
         
-        Arrays.sort(skill);
-        int i=0, j= n-1;
-        long res=0;
-        while(i<n/2 && j>=n/2 && i!=j){
-            if(skill[i]+skill[j]!=req) return -1;
-            else{
-                res+= (skill[i]*skill[j]);   
-            }
-            i++;j--;
+        // Arrays.sort(skill);
+        long res= 0;
+        for(int s : skill){
+            int tmp= req- s;
+            if(freq[tmp]==0) return -1;
+            res+= (long) s* (long) tmp;
+            freq[tmp]--;
         }
-        return res;
+        
+        return res/2;
     }
 }
