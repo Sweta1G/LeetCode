4058aa94a255bf8422519dc27bdddd3fa7cb127a
Sweta Gupta <122213106+Sweta1G@users.noreply.github.com>
2024-10-11 22:05:03 +0530

    Time: 65 ms (26.90%), Space: 53.5 MB (7.02%) - LeetHub

--- 1942-the-number-of-the-smallest-unoccupied-chair/1942-the-number-of-the-smallest-unoccupied-chair.java
+++ 1942-the-number-of-the-smallest-unoccupied-chair/1942-the-number-of-the-smallest-unoccupied-chair.java
@@ -2,26 +2,22 @@ class Solution {
     public int smallestChair(int[][] times, int targetFriend) {
         int n = times.length;
 
-        // List of events (time, friendIndex, type: 1 for arrival, 0 for departure)
         List<int[]> events = new ArrayList<>();
         for (int i = 0; i < n; i++) {
-            events.add(new int[] {times[i][0], i, 1});  // Arrival event
-            events.add(new int[] {times[i][1], i, 0});  // Departure event
+            events.add(new int[] {times[i][0], i, 1});
+            events.add(new int[] {times[i][1], i, 0});  
         }
 
-        // Sort events: by time first, if same time, prioritize arrivals over departures
         Collections.sort(events, (a, b) -> {
-            if (a[0] != b[0]) return a[0] - b[0];   // Compare by time
-            return a[2] - b[2];                     // Arrival (1) before departure (0)
+            if (a[0] != b[0]) return a[0] - b[0];
+            return a[2] - b[2];                   
         });
 
-        // Min-heap to track available chairs
         PriorityQueue<Integer> availableChairs = new PriorityQueue<>();
         for (int i = 0; i < n; i++) {
             availableChairs.offer(i);
         }
 
-        // Map to track which chair each friend is sitting on
         Map<Integer, Integer> friendToChair = new HashMap<>();
 
         // Process each event
@@ -31,22 +27,18 @@ class Solution {
             int isArrival = event[2];
 
             if (isArrival == 1) {
-                // Friend arrives, assign the smallest available chair
                 int assignedChair = availableChairs.poll();
                 friendToChair.put(friend, assignedChair);
 
-                // If this is the target friend, return their chair number
                 if (friend == targetFriend) {
                     return assignedChair;
                 }
             } else {
-                // Friend leaves, free up their chair
                 int chairToFree = friendToChair.get(friend);
                 availableChairs.offer(chairToFree);
             }
         }
 
-        // This should never be reached since the targetFriend will be found during processing
         return -1;
     }
     
