c6ebb3934ed8983834af49e9eeb28cb549ad5006
Sweta Gupta <122213106+Sweta1G@users.noreply.github.com>
2024-10-14 11:01:38 +0530

    Time: 155 ms (40.28%), Space: 63 MB (60.66%) - LeetHub

--- 2530-maximal-score-after-applying-k-operations/2530-maximal-score-after-applying-k-operations.java
+++ 2530-maximal-score-after-applying-k-operations/2530-maximal-score-after-applying-k-operations.java
@@ -1,23 +1,14 @@
 class Solution {
     public long maxKelements(int[] nums, int k) {
         long res= 0;
-        // PriorityQueue<int[]> pq= new PriorityQueue<>((a,b) -> Integer.compare(b[0],a[0]));
         PriorityQueue<Integer> pq=  new PriorityQueue<>(Comparator.reverseOrder());
-        // PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
-        
         for(int i=0;i<nums.length;i++){
-            // pq.add(new int[]{nums[i],i});
             pq.add(nums[i]);
         }
         while(k-->0){
-//             int curr[]= pq.poll();
-//             res+= curr[0];
-        
-//             nums[curr[1]]= (int) Math.ceil((curr[0]*1.0)/3);
-//             pq.add(new int[]{nums[curr[1]],curr[1]});
-            int ele = pq.poll();  // Extract the max element
+            int ele = pq.poll();
             res += ele;
-            pq.add((int) Math.ceil(ele / 3.0));  // Re-insert ceil(ele / 3) into the queue
+            pq.add((int) Math.ceil(ele / 3.0));
         }
         return res;
     }
