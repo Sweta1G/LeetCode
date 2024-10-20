3b8497fb4047ef3c12758866a63a40479450325f
Sweta Gupta <122213106+Sweta1G@users.noreply.github.com>
2024-10-20 14:51:19 +0530

    Time: 15 ms (26.95%), Space: 45.1 MB (10.08%) - LeetHub

--- 1106-parsing-a-boolean-expression/1106-parsing-a-boolean-expression.java
+++ 1106-parsing-a-boolean-expression/1106-parsing-a-boolean-expression.java
@@ -26,7 +26,6 @@ class Solution {
     
     
     public static boolean parseBoolExpr(String exp) {
-        // int n= exp.length();
         Stack<Character> st= new Stack<>();
         for(char c: exp.toCharArray()){
             if(c==')'){
@@ -36,14 +35,12 @@ class Solution {
                 st.pop();
                 // e= e.reverse();
                 char operation= st.pop();
-                System.out.println("Expression : "+e+"\tOperation: "+operation);
                 char evaluated= '#'; 
                 switch(operation){
                     case '&': evaluated= parse_and(e.toString()); break;
                     case '|': evaluated= parse_or(e.toString()); break;
                     case '!': evaluated= parse_not(e.toString()); break;
                 }
-                System.out.println("Evaluated: "+evaluated);
                 st.push(evaluated);
             }
             else st.push(c);
