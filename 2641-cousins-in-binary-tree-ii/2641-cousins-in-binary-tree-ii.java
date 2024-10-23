677be150d465030515c69547d0e1b8a3db0406df
Sweta Gupta <122213106+Sweta1G@users.noreply.github.com>
2024-10-23 08:22:06 +0530

    Time: 30 ms (47.46%), Space: 73.7 MB (91.53%) - LeetHub

---
+++ 2641-cousins-in-binary-tree-ii/2641-cousins-in-binary-tree-ii.java
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode replaceValueInTree(TreeNode root) {
+        dfs(new TreeNode[]{root});
+        root.val= 0;
+        return root;
+    }
+    private void dfs(TreeNode[] arr){
+        if(arr.length==0) return;
+        
+        int sum= 0;
+        for(TreeNode node : arr){
+            if(node==null) continue;
+            if(node.left!=null) sum+= node.left.val;
+            if(node.right!=null) sum+= node.right.val;
+        }
+        TreeNode childArr[]= new TreeNode[arr.length*2];
+        int index= 0;
+        for(TreeNode node : arr){
+            int currSum= 0;
+            if(node.left!=null) currSum+= node.left.val;
+            if(node.right!=null) currSum+=node.right.val;
+            
+            if(node.left!=null) {node.left.val= sum-currSum; childArr[index++]= node.left;}
+            if(node.right!=null) {node.right.val= sum-currSum; childArr[index++]= node.right;}
+        }
+        
+        dfs(Arrays.copyOf(childArr,index));
+    }
+}
