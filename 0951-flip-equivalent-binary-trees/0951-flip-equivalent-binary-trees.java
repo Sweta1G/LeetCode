af23a88b4ad7210a9795050584321b159573be15
Sweta Gupta <122213106+Sweta1G@users.noreply.github.com>
2024-10-24 14:55:27 +0530

    Time: 0 ms (100.00%), Space: 41.4 MB (32.68%) - LeetHub

--- 0951-flip-equivalent-binary-trees/0951-flip-equivalent-binary-trees.java
+++ 0951-flip-equivalent-binary-trees/0951-flip-equivalent-binary-trees.java
@@ -13,20 +13,11 @@
  *     }
  * }
  */
-class Solution { public boolean flipEquiv(TreeNode root1, TreeNode root2) {
+class Solution {
+    public boolean flipEquiv(TreeNode n1, TreeNode n2) {
+        if(n1==null && n2== null) return true;
+        if(n1==null || n2==null || n1.val!=n2.val) return false;
         
-        return checker(root1, root2);
-    }
-
-    private boolean checker(TreeNode node1, TreeNode node2) {
-        if (node1 == null && node2 == null) {
-            return true;
-        }
-        if (node1 == null || node2 == null || node1.val != node2.val) {
-            return false;
-        }
-
-        return (checker(node1.left, node2.left) || checker(node1.left, node2.right)) &&
-               (checker(node1.right, node2.right) || checker(node1.right, node2.left));
+        return (flipEquiv(n1.left,n2.left) || flipEquiv(n1.left,n2.right)) && (flipEquiv(n1.right,n2.right)||flipEquiv(n1.right,n2.left));
     }
 }
