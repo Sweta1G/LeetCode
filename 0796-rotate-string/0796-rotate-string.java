9bc0b1c46ac12ac093e566bc048b97c275a5c461
Sweta Gupta <122213106+Sweta1G@users.noreply.github.com>
2024-11-03 10:46:10 +0530

    Time: 0 ms (100.00%), Space: 41.7 MB (20.30%) - LeetHub

---
+++ 0796-rotate-string/0796-rotate-string.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean rotateString(String s, String goal) {
+        if(s.length()!=goal.length()) return false;
+        if(s.equals(goal)) return true;
+        
+        StringBuilder sb= new StringBuilder(s);
+        sb.append(s);
+        for(int i=0; i<s.length(); i++){
+            if(sb.substring(i,i+s.length()).equals(goal)) return true;
+        }
+        return false;
+    }
+}
