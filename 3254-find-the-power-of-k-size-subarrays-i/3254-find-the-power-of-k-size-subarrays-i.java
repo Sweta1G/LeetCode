ee70a86a516ab11ad57877cd621f9b9765d7951e
Sweta Gupta <122213106+Sweta1G@users.noreply.github.com>
2024-11-16 06:51:31 +0530

    Time: 2 ms (58.78%), Space: 45.8 MB (6.12%) - LeetHub

--- 3254-find-the-power-of-k-size-subarrays-i/3254-find-the-power-of-k-size-subarrays-i.java
+++ 3254-find-the-power-of-k-size-subarrays-i/3254-find-the-power-of-k-size-subarrays-i.java
@@ -1,8 +1,9 @@
 class Solution {
-    private boolean Valid(int nums[], int i, int j, int k){
-        for(int x=i; x<j; x++){
-            if(nums[x]+1!=nums[x+1]) return false;
-        }
+    private boolean Valid(int nums[], int i, int j){
+        for(int x=i; x<j; x++)
+            if(nums[x]+1!=nums[x+1]) 
+                return false;
+        
         return true;
     }
     public int[] resultsArray(int[] nums, int k) {
@@ -10,9 +11,10 @@ class Solution {
         int res[]= new int[n-k+1];
         while(j<n){
             if(j-i+1==k){
-                if(Valid(nums,i,j,k))
+                if(Valid(nums,i,j))
                     res[i]= nums[j];
-                else res[i]= -1;
+                else 
+                    res[i]= -1;
                 i++;
             }
             j++;
