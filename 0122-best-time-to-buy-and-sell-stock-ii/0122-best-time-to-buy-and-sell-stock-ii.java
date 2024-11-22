316ae63a8be6f26449d8a5405555669d842a676a
Sweta Gupta <122213106+Sweta1G@users.noreply.github.com>
2024-11-22 21:27:06 +0530

    Time: 1 ms (92.13%), Space: 45.4 MB (93.02%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,26 +1,24 @@
 class Solution {
-    private int find(int arr[], int n, int[][] dp){
-        dp[n][0]=dp[n][1]= 0;
+    private int find(int arr[], int n){
+        int next[]= new int[2], curr[]= new int[2];
+        next[0]= next[1]= 0;
+        int res;
         for(int i=n-1; i>=0; i--){
-            int res;
             for(int b=0; b<2; b++){
                 if(b==0)
-                    // res=Math.max(0+find(arr,i+1,0,n,dp),-arr[i]+find(arr,i+1,1,n,dp));
-                    res= Math.max(0+dp[i+1][0],-arr[i]+dp[i+1][1]);
+                    res= Math.max(0+next[0],-arr[i]+next[1]);
                 else
-                    // res= Math.max(0+find(arr,i+1,1,n,dp),arr[i]+find(arr,i+1,0,n,dp));
-                    res= Math.max(0+dp[i+1][1],arr[i]+dp[i+1][0]);
-                dp[i][b]= res;
+                    res= Math.max(0+next[1],arr[i]+next[0]);
+                curr[b]= res;
             }
+            next[0]=curr[0];
+            next[1]= curr[1];
         }
-        return dp[0][0];
+        return curr[0];
     }
     public int maxProfit(int[] prices) {
-        int n= prices.length;
-        int dp[][]= new int[n+1][2];
-        for(int x[] : dp)
-            Arrays.fill(x,-1);
-        int res= find(prices,n,dp);
+        int n= prices.length;;
+        int res= find(prices,n);
         return res;
     }
 }
